a036e3a1e4cf5aba8f8e305af2d36e56ac1c5752
vagrant <vagrant@homestead>
2020-11-08 22:06:48 +0000

    Role based authentication part 2

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -13,6 +13,6 @@ class Role extends Model
 
     public function users()
     {
-      return $this->belongsToMany('App\Models\User');
+      return $this->belongsToMany('App\Models\User', 'user_role');
     }
 }
